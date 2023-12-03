6e0314b07da49836769b81cf537d2051db9b8463
myuito3 <m.yuito3@gmail.com>
2023-12-03 22:00:03 +0900

    Add functions for mathematical processing

---
+++ advgrads/adversarial/attacks/utils/math.py
@@ -0,0 +1,29 @@
+# Copyright 2023 Makoto Yuito. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Math helper functions."""
+
+import torch
+from torch import Tensor
+
+
+def normalize(vectors: Tensor, eps: float = 1e-10) -> Tensor:
+    """Returns normalized vectors.
+
+    Args:
+        vectors: Vectors to normalize.
+        eps: Epsilon value to avoid division by zero.
+    """
+    dims = list(range(vectors.ndim))
+    return vectors / (torch.norm(vectors, p=2, dim=dims[1:], keepdim=True) + eps)
