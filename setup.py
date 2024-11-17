cbaf1d3ec42484f020c2d7555061fce1d5507ec8
myuito3 <m.yuito3@gmail.com>
2024-11-17 06:31:37 +0000

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="advgrads",
-    version="0.1.0",
+    version="0.1.1",
     author="Makoto Yuito",
     description="AdvGrads - An all-in-one tool for adversarial attacks on image recognition",
     packages=find_packages(),
